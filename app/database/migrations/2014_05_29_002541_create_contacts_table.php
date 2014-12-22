2383a4260b65ec286036f163e783d1d74b8b3cf5
Chris Gmyr <cmgmyr@gmail.com>
2014-12-22 11:04:30 -0500

    formatting

--- app/database/migrations/2014_05_29_002541_create_contacts_table.php
+++ app/database/migrations/2014_05_29_002541_create_contacts_table.php
@@ -3,35 +3,33 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateContactsTable extends Migration {
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('contacts', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->string('first_name');
-			$table->string('last_name');
-			$table->string('email');
-			$table->string('phone');
-			$table->timestamps();
-		});
-	}
 
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('contacts');
-	}
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('contacts');
+    }
 }
